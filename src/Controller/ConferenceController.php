2af56c69df7fb5527bda917a1c44ddf71df90306
babs95 <babsco95@gmail.com>
2022-12-09 18:28:31 +0000

    Adding index,details view for conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Request $request): Response
+    public function index(LoggerInterface $logger, ConferenceRepository $conferenceRepository): Response
     {
-        $greet = '';
+        // retrieve the object from database
+        $conferences = $conferenceRepository->findAll();
+        if (!$conferences) {
+            throw $this->createNotFoundException('The conferences does not exist');
+            // the above is just a shortcut for:
+            // throw new NotFoundHttpException('The product does not exist');
+        }
+        /*$greet = '';
         if ($name = $request->query->get('hello')) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
@@ -22,7 +34,33 @@ class ConferenceController extends AbstractController
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
             'greet' => $greet,
-        ]);
+        ])*/
         //return new Response('<html><body> $greet <img src="/images/under-construction.gif" /></body></html>');
+
+        /*return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));*/
+
+        $logger->info('Conference Controller index called!');
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('conference/index.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'conferences' => $conferences,
+        ]);
+
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Conference $conference, CommentRepository $commentRepository): Response
+    {
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+        ]);
     }
 }
